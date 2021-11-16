8835bd40dcd10c50b17af31d4111cc8d2a9487ad
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2021-11-16 02:31:35 -0500

    Intentando que Firebase funcione

--- reactnotes/src/App.js
+++ reactnotes/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import db from "./config/config";
+
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      notes: [
+        
+      ]
+    };
+   
+    this.addNote = this.addNote.bind(this);
+  }
+
+  componentDidMount() {
+    const { notes } = this.state;
+    db.on('child_added', snap => {
+      notes.push({
+        noteId: snap.key,
+        noteContent: snap.val().noteContent
+      })
+      this.setState({notes});
+    });
+  }
+
+  removeNote() {
+
+  }
+
+  addNote(note) {
+    let { notes } = this.state;
+    notes.push({
+      noteId: note.length + 1,
+      noteContent: note
+    });
+    this.setState({ notes });
+  }
+
+  render() {
+    return(
+      <div className="notesContainer">
+        <div className="notesHeader">
+          <h1>React y Firebase App</h1>
+        </div>
+
+        <div className="notesBody">
+          <ul>
+            {
+            this.state.notes.map(note =>{
+              return (
+                <Note
+                noteContent = {note.noteContent}
+                noteId = {note.noteId}
+                key = {note.noteId}
+                />
+              )
+          })
+          }
+          </ul>
+        </div>
+
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote}/>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
